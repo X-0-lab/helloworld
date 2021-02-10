5e9bee590731da9421e2c0a7ce91a53de3b420ed
Andrew Niemantsverdriet <andrewNiemants@gmail.com>
2021-02-10 11:38:11 -0700

    initial commit

---
+++ app/Http/Controllers/TalkController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Talk;
+use Illuminate\Http\Request;
+
+class TalkController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $talks = Talk::all();
+        return view('talk.index', compact('talks'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('talk.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Talk::create([
+            'title' => $request->title,
+            'description' => $request->description,
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Talk  $talk
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Talk $talk)
+    {
+        return view('talk.show', compact('talk'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Talk  $talk
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Talk $talk)
+    {
+        return view('talk.edit', compact('talk'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Talk  $talk
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Talk $talk)
+    {
+        $talk->title = $request->title;
+        $talk->description = $request->description;
+        $talk->save();
+        redirect('/talk/'.$talk->id);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Talk  $talk
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Talk $talk)
+    {
+        $talk->delete();
+        redirect('/talk');
+    }
+}
