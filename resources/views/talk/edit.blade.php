ab183fcc1c632e818cec384ce134e08bde56958c
Andrew Niemantsverdriet <andrewNiemants@gmail.com>
2021-02-12 19:09:37 -0700

    Feb 12 commit

---
+++ resources/views/talk/edit.blade.php
@@ -0,0 +1,16 @@
+<form method="post" action="/talk/{{ $talk->id }}">
+    @csrf
+    @method('patch')
+    <div>
+        Title: <input type="text" name="title" value="{{ old('title', $talk->title) }}">
+        @error('title'){{ $message }}@enderror
+    </div>
+
+    <div>
+        Description:
+        <textarea name="description">{{ old('description', $talk->description) }}</textarea>
+        @error('description'){{ $message }}@enderror
+    </div>
+
+    <button type="submit">Save</button>
+</form>
